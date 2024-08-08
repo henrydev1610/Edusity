feca67d68115e7ff0889df8e0c3686fb5037d40f
henrydev1610 <henrydev1610>
2024-08-08 12:21:45 -0300

    commit final

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,7 +1,8 @@
 import './navbar.css'
+
 import logo from '../../assets/logo.png'
 import { useState, useEffect } from 'react'
-
+import { Link } from 'react-scroll'
 const navbar = () => {
 
   const [sticky, setSticky] = useState(false);
@@ -20,12 +21,12 @@ const navbar = () => {
     <nav className={`container ${sticky? 'dark-navbar': ''}`}>
         <img src={logo} alt="logo_página" className='logo' />
         <ul>
-            <li> <a href='#/'>Home</a> </li>
-            <li> <a href='#/'>Programa</a> </li>
-            <li> <a href='#/'>Sobre Nós</a></li>
-            <li> <a href='#/'>Campus</a></li>
+            <li> <a href='#/'><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></a></li>
+            <li> <a href='#/'><Link to='program' smooth={true} offset={0} duration={500} >Program</Link></a> </li>
+            <li> <a href='#/'><Link to='about' smooth={true} offset={0} duration={500} >Sobre Nós</Link></a></li>
+            <li> <a href='#/'><Link to='campus' smooth={true} offset={0} duration={500} >Campus</Link></a></li>
             <li> <a href='#/'>Testemunhos</a></li>
-            <li> <a href='#/'><button className='btn'>Contate-nos</button></a></li>
+            <li> <a href='#/'><Link to='contact' smooth={true} offset={0} duration={500} >Contate-nos</Link></a></li>
         </ul>
 
 
